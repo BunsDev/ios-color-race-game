7ce047477dc73fba0d8d3b8fa3c2038304cbbc5f
Anup D'Souza <103429618+anupdsouza@users.noreply.github.com>
2023-08-16 10:11:35 +0530

    Add initial project files

---
+++ ColorRace/ColorRace/ColorRaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorRaceApp.swift
+//  ColorRace
+//
+//  Created by Anup D'Souza on 16/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ColorRaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
