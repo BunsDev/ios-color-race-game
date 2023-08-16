e81b482c74ca0422d20e91bdcbeb6f96ac4a234a
Anup D'Souza <103429618+anupdsouza@users.noreply.github.com>
2023-08-16 15:12:20 +0530

    Update content view to show tiles

--- ColorRace/ColorRace/ContentView.swift
+++ ColorRace/ColorRace/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 10) {
+            ForEach(0..<3) { _ in
+                HStack(spacing: 10) {
+                    ForEach(0..<3) { _ in
+                        TileView()
+                    }
+                }
+            }
         }
         .padding()
+    }
+}
+
+struct TileView: View {
+    @State private var colorIndex = 0
+    private let colors: [Color] = [.white, .red, .green, .blue]
+    
+    var body: some View {
+        Rectangle()
+            .fill(colors[colorIndex])
+            .frame(width: 100, height: 100)
+            .border(.black, width: 2.0)
+            .onTapGesture {
+                colorIndex = (colorIndex + 1) % colors.count
+            }
     }
 }
 
