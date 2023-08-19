b27d93574b88cd07ba368ef8b61aecdbc148cf38
Anup D'Souza <103429618+anupdsouza@users.noreply.github.com>
2023-08-19 11:32:28 +0530

    Add broadcasting events examples

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const app = express();
+const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 const port = 3000
+var users = 0
 
 // app.listen(port, function() {
 //     console.log(`App running on http://localhost:${port}`)
@@ -20,7 +21,25 @@ http.listen(port, function() {
 io.on('connection', function(socket) {
     console.log('someone connected!');
 
+    users++
+    // emit message to all connected users
+    // io.sockets.emit('broadcastActiveUsers', { message: users + " connected"})
+
+    // emit message to new connections
+    socket.emit('newUser', { message: "Welcome!"})
+
+    // emit message to existing connections
+    socket.broadcast.emit('newUser', { message: users + " connected"})
+
     socket.on('disconnect', function() {
         console.log('someone disconnected!');
+
+        users--
+
+        // emit message to all connected users
+        // io.sockets.emit('broadcastActiveUsers', { message: users + " connected"})
+
+        // emit message to existing connections
+        socket.broadcast.emit('newUser', { message: users + " connected"})
     });
 });
